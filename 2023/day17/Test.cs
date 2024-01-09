dce2af4bcae5bd0ca2e9bf659fcd65f3dc8ef1cb
Vincent Bitter <vincent@vincentbitter.nl>
2024-01-09 21:45:02 +0100

    Cleanup day 17

--- 2023/day17/Test.cs
+++ 2023/day17/Test.cs
@@ -13,17 +13,18 @@ public class Test
     {
         var input = Parser.ReadAllLines(fileName);
         var positions = new Position[input.Length][];
-        for(var y = 0; y < input.Length; y++)
+        for (var y = 0; y < input.Length; y++)
         {
             positions[y] = new Position[input[0].Length];
-            for(var x = 0; x < input[0].Length; x++)
+            for (var x = 0; x < input[0].Length; x++)
             {
                 positions[y][x] = new Position(x, y, int.Parse("" + input[y][x]));
             }
         }
         foreach (var a in positions)
         {
-            foreach(var p in a) {
+            foreach (var p in a)
+            {
                 p.SetCanComeFrom(positions, min, max);
             }
         }
@@ -35,13 +36,17 @@ public class Test
         start.VerticalDirty = true;
         start.HorizontalDirty = true;
         queue.Enqueue(start);
-        while (queue.Count > 0) {
+        while (queue.Count > 0)
+        {
             var item = queue.Dequeue();
-            if (item.HorizontalDirty) {
+            if (item.HorizontalDirty)
+            {
                 var destinations = item.CanComeFromHorizontal;
-                foreach (var dest in destinations) {
+                foreach (var dest in destinations)
+                {
                     var cost = item.CostVertical + dest.GetCost(item);
-                    if (cost < dest.CostHorizontal || dest.CostHorizontal == 0) {
+                    if (cost < dest.CostHorizontal || dest.CostHorizontal == 0)
+                    {
                         dest.CostHorizontal = cost;
                         dest.VerticalDirty = true;
                         queue.Enqueue(dest);
@@ -49,11 +54,14 @@ public class Test
                 }
                 item.HorizontalDirty = false;
             }
-            if (item.VerticalDirty) {
+            if (item.VerticalDirty)
+            {
                 var destinations = item.CanComeFromVertical;
-                foreach (var dest in destinations) {
+                foreach (var dest in destinations)
+                {
                     var cost = item.CostHorizontal + dest.GetCost(item);
-                    if (cost < dest.CostVertical || dest.CostVertical == 0) {
+                    if (cost < dest.CostVertical || dest.CostVertical == 0)
+                    {
                         dest.CostVertical = cost;
                         dest.HorizontalDirty = true;
                         queue.Enqueue(dest);
@@ -68,10 +76,11 @@ public class Test
     }
 }
 
-public class Position {
-    public int X {get;set;}
-    public int Y {get;set;}
-    public int Value {get;set;}
+public class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Value { get; set; }
 
     public Position(int x, int y, int value)
     {
@@ -83,53 +92,61 @@ public class Position {
     public List<Position> CanComeFromHorizontal { get; private set; } = new List<Position>();
     public List<Position> CanComeFromVertical { get; private set; } = new List<Position>();
 
-    public bool HorizontalDirty {get;set;}
-    public bool VerticalDirty {get;set;}
+    public bool HorizontalDirty { get; set; }
+    public bool VerticalDirty { get; set; }
 
     public int CostHorizontal { get; internal set; }
     public int CostVertical { get; internal set; }
 
-    public void SetCanComeFrom(Position[][] positions, int min, int max) {
+    public void SetCanComeFrom(Position[][] positions, int min, int max)
+    {
         var total = 0;
-        for (var x = X + 1; x <= Math.Min(positions[0].Length - 1, X + max); x++) {
+        for (var x = X + 1; x <= Math.Min(positions[0].Length - 1, X + max); x++)
+        {
             var pos = positions[Y][x];
             if (pos == null)
                 break;
             total += pos.Value;
 
-            if (x >= X + min) {
-                CostCache.Add(pos, total);
+            if (x >= X + min)
+            {
+                Cache(pos, total);
                 CanComeFromHorizontal.Add(pos);
-
-                pos.CostCache.Add(this, total - pos.Value + Value);
                 pos.CanComeFromHorizontal.Add(this);
             }
         }
         total = 0;
-        for (var y = Y + 1; y <= Math.Min(positions.Length - 1, Y + max); y++) {
+        for (var y = Y + 1; y <= Math.Min(positions.Length - 1, Y + max); y++)
+        {
             var pos = positions[y][X];
             if (pos == null)
                 break;
             total += pos.Value;
 
-            if (y >= Y + min) {
-                CostCache.Add(pos, total);
+            if (y >= Y + min)
+            {
+                Cache(pos, total);
                 CanComeFromVertical.Add(pos);
-
-                pos.CostCache.Add(this, total - pos.Value + Value);
                 pos.CanComeFromVertical.Add(this);
             }
         }
     }
 
-    public Dictionary<Position, int> CostCache {get; private set;} = new Dictionary<Position, int>();
+    private void Cache(Position pos, int total)
+    {
+        CostCache[pos] = total;
+        pos.CostCache[this] = total - pos.Value + Value;
+    }
+
+    public Dictionary<Position, int> CostCache { get; private set; } = new Dictionary<Position, int>();
     public int GetCost(Position position)
     {
         return CostCache[position];
     }
 }
 
-public enum Direction {
+public enum Direction
+{
     Horizontal,
     Vertical
 }
